cb9137bc94103b4c545daed34779550719ba1cb3
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-31 15:57:02 +0200

    usng mpck API to fetch data and show it. todos

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
 				<Link href={"/"}>Home</Link>
 				<Link href={"/about"}>About</Link>
 				<Link href={"/contact"}>Contact</Link>
+				<Link href={"/count"}>Count</Link>
 			</header>
 		</div>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-// allow us to use hooks and etc.
-"use client";
+export default async function Home() {
+	// Dan normaly make his prisma quiries here and talks diecty to DB
+	const todos = await fetch("https://jsonplaceholder.typicode.com/todos/").then(
+		(res) => res.json()
+	);
 
-import { useState } from "react";
-
-export default function Home() {
-	// !!! this will lead to an error therefore this page which is a client component,
-	// supposed to be marked on top that is supposed to be interactive on the client side,
-	// mainly because of js (onClick listiner and useState hook, which are iteractive)
-	const [count, setCount] = useState(0);
 	return (
-		<div className='font-extrabold text-xl'>
-			<span>Homepage</span>
-			<div className='flex flex-col gap-2 ml-6 justify-items-center'>
-				<p>{count}</p>
-				<div className='flex flex-grow gap-6'>
-					<button onClick={() => setCount(count - 1)}>-</button>
-					<button onClick={() => setCount(count + 1)}>+</button>
-				</div>
-			</div>
+		<div className='flex flex-col'>
+			<span className='font-extrabold text-xl'>Homepage</span>
+			<ul className='grid grid-cols-4 gap-4'>
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
